e27cd4adeb59ec3db284cbd913b42e3af5c789f1
enonibobble <laidbackenon@outlook.com>
2026-05-29 18:20:32 +0100

    WIP

--- Crunchymatic.Web/Models/Anime.cs
+++ Crunchymatic.Web/Models/Anime.cs
@@ -2,17 +2,16 @@
 
 namespace Crunchymatic.Web.Models;
 
-public record Anime
+public class Anime
 {
-    [Key]
-    public int Id { get; set; }
-    
-    public required string EnglishTitle { get; set; }
-    
-    public string? SheetName { get; set; }
-    
-    public required string CrunchyrollSeasonId { get; set; }
-    public required string CrunchyrollSeriesId { get; set; }
-    
+    [Key] public int Id { get; set; }
+
+    [MaxLength(256)] public required string EnglishTitle { get; set; }
+
+    [MaxLength(128)] public string? SheetName { get; set; }
+
+    [MaxLength(32)] public required string CrunchyrollSeasonId { get; set; }
+    [MaxLength(32)] public required string CrunchyrollSeriesId { get; set; }
+
     public ICollection<EpisodeCheck> EpisodeChecks { get; set; } = [];
 }
--- Crunchymatic.Web/Models/EpisodeCheck.cs
+++ Crunchymatic.Web/Models/EpisodeCheck.cs
@@ -5,24 +5,23 @@ namespace Crunchymatic.Web.Models;
 
 public class EpisodeCheck
 {
-    [Key]
-    public int Id { get; set; }
-    
+    [Key] public int Id { get; set; }
+
     public NodaTime.Instant EpisodeReleased { get; set; }
 
-    public required int EpisodeNumber { get; set; }
+    [MinLength(1), MaxLength(16)] public required string Episode { get; set; }
 
-    [MaxLength(8)]
-    public string AudioLanguage { get; set; } = "jpn";
-    
-    
+    [MinLength(1), MaxLength(8)] public string AudioLanguage { get; set; } = "jpn";
+
+    [MaxLength(32)] public required string CrunchyrollEpisodeId { get; set; }
+
+
     public NodaTime.Instant TimeSubtitlesGrabbed { get; set; }
-    
-    public string? SubtitleArchivePath { get; set; }
-    
+
+    [MaxLength(256)] public string? SubtitleArchivePath { get; set; }
+
     public ICollection<CheckedSubtitle> CheckedSubtitles { get; set; } = [];
-    
-    [ForeignKey(nameof(LinkedAnime))]
-    public int AnimeId { get; set; }
+
+    [ForeignKey(nameof(LinkedAnime))] public int AnimeId { get; set; }
     public Anime? LinkedAnime { get; set; }
 }
